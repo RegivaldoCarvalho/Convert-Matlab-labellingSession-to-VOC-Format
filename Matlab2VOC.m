fe0501bf05e041faeee4e6b87f40691687f32ddd
Regivaldo Carvalho <42656775+RegivaldoCarvalho@users.noreply.github.com>
2018-09-19 20:43:29 -0300

    Add files via upload

--- Matlab2VOC.m
+++ Matlab2VOC.m
@@ -1,7 +1,7 @@
 % Convert Matlab Lalbeling Session to a VOC Label
 
 % Load labelingSesssion
-load('D:\Python\Power_Grid_Inspection\labelingSessions\Isolador_Pilar_01_09_2018.mat')
+load('D:\Python\Power_Grid_Inspection\labelingSessions\Isolador_Pilar_02_09_2018.mat')
 % Defina a fote dos dados
 Fonte = 'Power_Line_Vale'; 
 
@@ -10,38 +10,37 @@ len = size(labelingSession.ImageSet.ImageStruct,2);
 
 for i=1:len
     
-    path = labelingSession.ImageSet.ImageStruct(i).imageFilename; % resgata o endereÃ§o completo da foto
+    path = labelingSession.ImageSet.ImageStruct(i).imageFilename; % resgata o endereço completo da foto
     VOC.annotation.path.Text = path;
     % Separa o texto
     expression = '\\';
     splitStr = regexp(path,expression,'split');    
     % Nome da foto
-    foto = string(splitStr(size(splitStr,2)));;
-    VOC.annotation.filename.Text = foto; 
-    % Localiza o diretÃ³rio
+    foto = string(splitStr(size(splitStr,2)));
+    VOC.annotation.filename.Text = char(foto); 
+    % Localiza o diretório
     folder = string(splitStr(size(splitStr,2)-1));
-    VOC.annotation.folder.Text = folder  % Nome do diretÃ³rio da foto
+    VOC.annotation.folder.Text = char(folder);  % Nome do diretório da foto
     VOC.annotation.source.database.Text = Fonte; % Define a Fonte dos dados
-    % CaracterÃ­sticas da Imagem
+    % Características da Imagem
     [height, width, depth] = size(imread(path));
     VOC.annotation.size.width.Text = num2str(width);
     VOC.annotation.size.height.Text = num2str(height);
     VOC.annotation.size.depth.Text = num2str(depth);
-    % SegmentaÃ§Ã£o
+    % Segmentação
     VOC.annotation.segmented.Text = '0'
     
     
     number_bboxes = size(labelingSession.ImageSet.ImageStruct(i).objectBoundingBoxes,1);
     
     for j=1:number_bboxes    
-        i
-        j
+              
         % Objetos - Boundboxes
         VOC.annotation.object{1, j}.name.Text = labelingSession.CategorySet.CategoryStruct.categoryName; % Nome da Tag
         VOC.annotation.object{1, j}.pose.Text = 'Unspecified'; 
         VOC.annotation.object{1, j}.truncated.Text = '0';
         VOC.annotation.object{1, j}.difficult.Text = '0';
-        % PosiÃ§Ã£o da BoundBox  [x,y,width, height]
+        % Posição da BoundBox  [x,y,width, height]
         xmin = labelingSession.ImageSet.ImageStruct(i).objectBoundingBoxes(j); 
         ymin = labelingSession.ImageSet.ImageStruct(i).objectBoundingBoxes(j+ number_bboxes);
         xmax = xmin + labelingSession.ImageSet.ImageStruct(i).objectBoundingBoxes(j+(2*number_bboxes));
@@ -52,8 +51,10 @@ for i=1:len
         VOC.annotation.object{1, j}.bndbox.ymax.Text = ymax;
                 
     end
-     
-    nome = sprintf('%s.xml',foto);
+    
+    expression = '\.';
+    foto1 = regexp(foto,expression,'split');
+    nome = sprintf('%s.xml',foto1(1));
     struct2xml(VOC,nome);
     clear VOC;
     
